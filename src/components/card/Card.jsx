c9361fd3821c69d1b0db3cb9929417e92c209456
Yohannes Misganaw Assefa <Yomifalsa@gamil.com>
2024-08-16 06:02:18 +0300

    update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,36 +1,37 @@
 import { Card } from "antd";
+import "./Card.css";
 import { servicesData } from "../../container/Service/data";
 
 const { Meta } = Card;
+
 const CardImg = function () {
   return (
-    <>
-      <section className="services-section">
-        <div className="services-content">
-          <div className="services-grid">
-            {servicesData.map((service, index) => (
-              <div key={index} className="">
-                <Card
-                  hoverable
-                  style={{
-                    width: 240,
-                  }}
-                  cover={
-                    <img
-                      style={{ height: "190px" }}
-                      alt={service.title}
-                      src={service.imgSrc}
-                    />
-                  }
-                >
-                  <Meta title={service.title} description={service.content} />
-                </Card>
-              </div>
-            ))}
-          </div>
+    <section className="services-section">
+      <div className="services-content">
+        <div className="services-grid">
+          {servicesData.map((service, index) => (
+            <Card
+              key={index}
+              hoverable
+              style={{
+                width: "100%",
+                maxWidth: "240px",
+                margin: "0 auto", // Center the card
+              }}
+              cover={
+                <img
+                  style={{ height: "190px", width: "100%", objectFit: "cover" }}
+                  alt={service.title}
+                  src={service.imgSrc}
+                />
+              }
+            >
+              <Meta title={service.title} description={service.content} />
+            </Card>
+          ))}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
